8429f32c7ca12e693f78bb37d72e7d393898bf91
christorange <christol.yu@outlook.com>
2023-03-27 20:19:37 -0400

    update cv

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,7 @@ import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
-import resume from '../../../assets/CV_Chenfei_Yu.pdf'
+import resume from '../../../assets/Resume_Chenfei_Yu.pdf'
 
 const About = () => {
   const { name, role, description, social } = about
